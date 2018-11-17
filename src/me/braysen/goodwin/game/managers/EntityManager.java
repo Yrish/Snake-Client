f806fa9eb9f1683c98222aeba1f8a83af7d724fa
Jacob Peterson <git@jacobpeterson.net>
2018-11-16 17:00:01 -0700

    Fixed gitignore

--- src/me/braysen/goodwin/game/managers/EntityManager.java
+++ src/me/braysen/goodwin/game/managers/EntityManager.java
@@ -30,27 +30,4 @@ public class EntityManager {
     public void setFoods(ArrayList<Food> foods) {
         this.foods = foods;
     }
-
-    public void add(Entity e) {
-        if (entities.size() == 0) {
-            entities.add(e);
-        }
-        int max = entities.size();
-        int min = 0;
-        while (true) {
-            int cur = (max + min) / 2;
-            int val = e.compareTo(entities.get(cur));
-            if (val == 0) {
-                entities.set(cur, e);
-                return;
-            } else if (val > 0) {
-                max = cur - 1;
-            } else {
-                min = cur + 1;
-            }
-            if (min >= max) {
-                entities.set((min+max)/2, e);
-            }
-        }
-    }
 }
